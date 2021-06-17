455856878c999d4d7718ebcaadfed5bb86fedd12
NeonJonn <jonathan.groult1@gmail.com>
2021-06-17 12:39:31 +0100

    Updated InvenT

--- invent/script.js
+++ invent/script.js
@@ -1,19 +1,21 @@
 //Get the button
-var mybutton = document.getElementById("toTopButton");
+var mybutton = document.getElementById("topButton");
+
+document.getElementById("demo").innerHTML = "Testing please work.";
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
